a75559d958024f5c207c6bce2a5f2b7c461dbbb3
marcochow_mbp <26892819+iammarcode@users.noreply.github.com>
2025-04-05 10:59:55 +0800

    refine

--- scripts/terraform-backend.sh
+++ scripts/terraform-backend.sh
@@ -1,16 +1,10 @@
+REGION="ap-east-1"
+
 # S3
-aws s3api create-bucket --bucket ai-chat-bot-terraform-state --region ap-east-1 --create-bucket-configuration LocationConstraint=ap-east-1
-aws s3api put-bucket-versioning --bucket ai-chat-bot-terraform-state --versioning-configuration Status=Enabled --region ap-east-1
-aws s3api put-bucket-encryption --bucket ai-chat-bot-terraform-state --server-side-encryption-configuration '{"Rules":[{"ApplyServerSideEncryptionByDefault":{"SSEAlgorithm":"AES256"}}]}' --region ap-east-1
+aws s3api create-bucket --bucket ai-chat-bot-terraform-state --region ${REGION} --create-bucket-configuration LocationConstraint=ap-east-1
+aws s3api put-bucket-versioning --bucket ai-chat-bot-terraform-state --versioning-configuration Status=Enabled --region ${REGION}
+aws s3api put-bucket-encryption --bucket ai-chat-bot-terraform-state --server-side-encryption-configuration '{"Rules":[{"ApplyServerSideEncryptionByDefault":{"SSEAlgorithm":"AES256"}}]}' --region ${REGION}
 
-# Dynamodb
-aws dynamodb create-table \
-  --table-name terraform-locks \
-  --attribute-definitions AttributeName=LockID,AttributeType=S \
-  --key-schema AttributeName=LockID,KeyType=HASH \
-  --billing-mode PAY_PER_REQUEST \
-  --region ap-east-1
+aws s3 ls s3://ai-chat-bot-terraform-state --region ${REGION}
 
-aws s3 ls s3://ai-chat-bot-terraform-state --region ap-east-1
-
-aws dynamodb describe-table --table-name terraform-locks --region ap-east-1
+#TODO: Dynamodb (locking)
