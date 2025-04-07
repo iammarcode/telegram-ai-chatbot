17016712e0e2b3cd76cc136f75a562e0ba8d818c
Marco <26892819+iammarcode@users.noreply.github.com>
2025-04-07 10:22:02 +0800

    fix env (#20)

--- start-docker.sh
+++ start-docker.sh
@@ -6,13 +6,13 @@ IMAGE_NAME="chat-bot-image"
 TAG="latest"
 MULTI_PLATFORM="linux/amd64,linux/arm64"
 
+cp .env.docker .env
+
 # Clear container and volume
 docker compose down -v
 
 # Build app image
 docker buildx build --platform ${MULTI_PLATFORM} --target docker -t "${IMAGE_NAME}:${TAG}" --load .
-
-cp .env.docker .env
 
 docker compose up -d
 
